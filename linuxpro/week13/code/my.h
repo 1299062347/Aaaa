7e8d744ada6a130ca194094d83760d43a5427065
xmh <1299062347@qq.com>
2019-12-23 14:12:58 +1400

    week13submit

---
+++ linuxpro/week13/code/my.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include<time.h>
+#include<signal.h>
+#include<fcntl.h>
+#include<errno.h>
